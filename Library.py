7aa48cd1adc954bcb8725205935df4ed15476bb2
Jesus Suarez <jsuarezho@cesde.net>
2025-03-24 13:46:59 -0500

    Edit books

--- Library.py
+++ Library.py
@@ -6,14 +6,14 @@ class Book:
         self.genre = genre
 
     def show_data(self):
-        return f"Titulo: {self.title}, Autor: {self.author}, Año: {self.year}, Genero: {self.genre}"
+        return f"Titulo: {self.title}, Autor: {self.author}, Año: {self.year}, Genero: {self.genre}".title()
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self):
-        title = input("Ingresa el titulo del libro: ")
+        title = input("Ingresa el titulo del libro: ").lower()
         author = input("Ingresa el autor: ")
         year = input("Ingresa el año de lanzamiento del libro: ")
         genre = input("Ingresa el genero del libro: ")
@@ -22,7 +22,7 @@ class Library:
         print("Libro añadido.")
 
     def remove_book(self):
-        title = input("Ingresa el titulo dle libro a remover: ")
+        title = input("Ingresa el titulo del libro a remover: ").lower()
         for book in self.books:
             if book.title == title:
                 self.books.remove(book)
@@ -31,7 +31,7 @@ class Library:
         print("Lirbo no encontrado.")
 
     def search_book(self):
-        title = input("Ingresa el titulo del libro a buscar: ")
+        title = input("Ingresa el titulo del libro a buscar: ").lower()
         for book in self.books:
             if book.title == title:
                 print(book.show_data())
@@ -45,6 +45,18 @@ class Library:
         for book in self.books:
             print(book.show_data())
             print("---")
+    
+    def edit_book(self):
+        title = input("Ingresa el titulo del libro a editar: ").lower()
+        for book in self.books:
+            if book.title == title:
+                book.title = input("Nuevo título: ")
+                book.author = input("Nuevo autor: ")
+                book.year = input("Nuevo año: ")
+                book.genre = input("Nuevo género: ")
+                print("Libro actualizado.")
+                return
+        print("Libro no encontrado.")
 
 def main_menu():
     library = Library()
@@ -55,7 +67,8 @@ def main_menu():
         print("2. Eliminar libro")
         print("3. Buscar libro")
         print("4. Mostrar libros")
-        print("5. Salir")
+        print("5. Editar libro")
+        print("6. Salir")
         option = int(input("Seleciona una opcion: "))
 
         if option == 1:
@@ -67,6 +80,8 @@ def main_menu():
         elif option == 4:
             library.list_books()
         elif option == 5:
+            library.edit_book()
+        elif option == 6:
             print("Hasta pronto")
             break
         else:
